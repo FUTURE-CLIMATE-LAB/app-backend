c90507541fc49620ff8f9758584d1c387179596b
Isaias <isaias0david@gmail.com>
2023-11-28 03:34:35 -0400

    swagger

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,32 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-
-// Routes
-import MsgRoutes from './routes/msg.routes'
 import mongoose from 'mongoose'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import MsgRoutes from './routes/msg.routes'
+import swaggerConfig from '../swagger.json'
+
 dotenv.config()
 
 const app = express()
 
-// database
+// Configuración de Swagger
+const specs = swaggerJsdoc(swaggerConfig)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+
+// Database
 mongoose.connect(process.env.DB_URI ?? '', {
   authSource: 'admin'
 })
-  .then(() => console.log('conectado'))
+  .then(() => console.log('Conectado a la base de datos'))
   .catch((error) => console.log(error))
 
-// Config
+// Configuración de Express
 app.use(express.json())
 app.use(cors())
 
-// Routes
+// Rutas
 app.use('/api/message', MsgRoutes)
 
 export default app
--- src/routes/msg.routes.ts
+++ src/routes/msg.routes.ts
@@ -6,7 +6,49 @@ import * as MessageService from '../services/message.services'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /api/message/create:
+ *   post:
+ *     summary: Crear un nuevo mensaje.
+ *     tags:
+ *       - Message
+ *     requestBody:
+ *       description: Datos del mensaje a crear.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/MessageInput'
+ *     responses:
+ *       200:
+ *         description: Éxito, devuelve el mensaje creado.
+ *         content:
+ *           application/json:
+ *             example:
+ *               nombre: "Ejemplo"
+ *               body: "Contenido del mensaje"
+ */
 router.post('/create', asyncHandler(MessageService.createMessage))
+
+/**
+ * @swagger
+ * /api/message/get:
+ *   get:
+ *     summary: Obtener la lista de mensajes.
+ *     tags:
+ *       - Message
+ *     responses:
+ *       200:
+ *         description: Éxito, devuelve la lista de mensajes.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - nombre: "Ejemplo1"
+ *                 body: "Contenido del mensaje 1"
+ *               - nombre: "Ejemplo2"
+ *                 body: "Contenido del mensaje 2"
+ */
 router.get('/get', asyncHandler(MessageService.messageList))
 
 export default router
